f407ea3397e0569cb6bfbf057b70a5a4d175e926
mohamedaa123654 <maabdel22@gmail.com>
2020-06-21 12:27:24 +0200

    add job views& url

--- project/settings.py
+++ project/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'job',
+    # 'category',
+
+    #my apps
 ]
 
 MIDDLEWARE = [
